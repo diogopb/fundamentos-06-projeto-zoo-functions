cbcfbd3ebeb1a6f601fe377e46572858c8620de3
Diogo Pinto <dpintobarbosa@gmail.com>
2023-11-06 09:28:39 -0300

    resolve requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const searchAnimal = data.species.find((specie) => specie.name === animal);
+  const searchAge = searchAnimal.residents.every((minimumAge) => minimumAge.age >= age);
+
+  if (searchAge) {
+    return true;
+  }
+  return false;
 };
 
 module.exports = getAnimalsOlderThan;
