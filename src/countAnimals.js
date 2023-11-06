f16020bc2e5de97d061358af6baae4b66c19d587
Diogo Pinto <dpintobarbosa@gmail.com>
2023-11-06 13:32:13 -0300

    resolve requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
-};
+  if (!animal) {
+    const counterAnimals = {};
+    data.species.forEach((specie) => {
+      counterAnimals[specie.name] = specie.residents.length;
+    });
+    return counterAnimals;
+  }
 
+  const searchAnimals = data.species.find((search) => animal.species === search.name);
+  if (!animal.sex) {
+    return searchAnimals.residents.length;
+  }
+
+  const countSpeciesAndSex = searchAnimals.residents.filter((resident) =>
+    resident.sex === animal.sex).length;
+  return countSpeciesAndSex;
+};
 module.exports = countAnimals;
