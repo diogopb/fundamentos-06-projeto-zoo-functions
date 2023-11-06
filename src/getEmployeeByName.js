349bb1c1a44adac77953f99b65cb600ff3f16091
Diogo Pinto <dpintobarbosa@gmail.com>
2023-11-06 10:36:56 -0300

    resolve requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+
+  const firstName = data.employees.find((employee) => employee.firstName === employeeName);
+  if (firstName) {
+    return firstName;
+  }
+
+  const lastName = data.employees.find((employee) => employee.lastName === employeeName);
+  if (lastName) {
+    return lastName;
+  }
 };
 
 module.exports = getEmployeeByName;
