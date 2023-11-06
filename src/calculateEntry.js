0b1caee52a5d0a4d363e7e77bd9360080740f9b2
Diogo Pinto <dpintobarbosa@gmail.com>
2023-11-06 16:17:26 -0300

    resolve requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,13 +1,31 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
-  const zoo = 0;
-  return zoo;
+  const countBuyerAge = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      countBuyerAge.child += 1;
+    } else if (entrant.age < 50) {
+      countBuyerAge.adult += 1;
+    } else {
+      countBuyerAge.senior += 1;
+    }
+  });
+  return countBuyerAge;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+
+  const totalBuyers = Object.values(countEntrants(entrants));
+  let total = 0;
+
+  total += totalBuyers[0] * data.prices.child;
+  total += totalBuyers[1] * data.prices.adult;
+  total += totalBuyers[2] * data.prices.senior;
+  return total;
 };
 
 module.exports = { calculateEntry, countEntrants };
