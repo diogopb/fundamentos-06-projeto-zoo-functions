592f8b639504b8b1fbcd953fbd7efb512be5c3a3
Diogo Pinto <dpintobarbosa@gmail.com>
2023-11-07 16:55:52 -0300

    resolve requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,77 @@
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+const { species, hours } = data;
+
+const findAnimalsOfDay = (day) => {
+  if (day === 'Monday') {
+    return 'The zoo will be closed!';
+  }
+  return species
+    .filter((animal) => animal.availability.includes(day))
+    .map((element) => element.name);
+};
+
+const generateHours = (day) => {
+  if (day === 'Monday') {
+    return 'CLOSED';
+  }
+  const dayInfo = hours[day];
+  return `Open from ${dayInfo.open}am until ${dayInfo.close}pm`;
+};
+
+const availabilityAnimal = (animal) => {
+  const foundAnimal = species.find((specie) => specie.name === animal);
+  if (foundAnimal) {
+    return foundAnimal.availability;
+  } return undefined;
+};
+
+const dailySchedule = () => {
+  const schedule = {};
+  for (const day of Object.keys(hours)) {
+    schedule[day] = {
+      officeHour: generateHours(day),
+      exhibition: findAnimalsOfDay(day),
+    };
+  } return schedule;
+};
+
+const defineType = (animal) => {
+  const isAnimal = species.some((specie) => specie.name === animal);
+  const isDay = Object.keys(hours).includes(animal);
+  if (isAnimal) {
+    return 'itsAnimal';
+  } if (isDay) {
+    return 'itsDay';
+  }
+  return false;
+};
+
+const generateScheduleDaily = (day) => {
+  if (day === 'Monday') {
+    const schedule = {
+      Monday: { officeHour: generateHours(day),
+        exhibition: findAnimalsOfDay(day) },
+    }; return schedule;
+  }
+  const schedule = {
+    [day]: {
+      officeHour: generateHours(day),
+      exhibition: findAnimalsOfDay(day),
+    },
+  };
+  return schedule;
+};
+
+const getSchedule = (animal) => {
+  const animalOrDay = defineType(animal);
+  if (!animal || animalOrDay === false) {
+    return dailySchedule();
+  } if (animalOrDay === 'itsAnimal') {
+    return availabilityAnimal(animal);
+  } if (animalOrDay === 'itsDay') {
+    return generateScheduleDaily(animal);
+  }
 };
 
 module.exports = getSchedule;
