fd503cf122bf016b3617c4552b3d360e99023e93
Diogo Pinto <dpintobarbosa@gmail.com>
2023-11-07 10:41:22 -0300

    resolve requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const searchEmployee = (idEmployee) => data.employees.find((employee) =>
+  employee.id === idEmployee);
+const searchSpecie = (employee) => employee.responsibleFor[0];
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const findIdAnimal = searchSpecie(searchEmployee(id));
+  const findInfosAnimal = data.species.find((animal) => animal.id === findIdAnimal);
+  const ages = findInfosAnimal.residents.sort((a, b) => b.age - a.age);
+  const oldestResident = ages[0];
+
+  const array = [oldestResident.name, oldestResident.sex, oldestResident.age];
+
+  return array;
 };
 
 module.exports = getOldestFromFirstSpecies;
